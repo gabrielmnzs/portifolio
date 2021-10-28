24be5e2b0cb880e283dded7e784bf211a2dd2647
Gabriel <gabriel.mnzs@hotmail.com>
2021-10-28 20:48:35 -0300

    add css responsive + social media

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.scss";
 
-import Navbar from "./components/Navbar";
-import { ReactComponent as Logo } from './assets/img/Logo.svg';
+import { ReactComponent as Logo } from "./assets/img/Logo.svg";
 
 function App() {
   return (
@@ -12,8 +11,32 @@ function App() {
           <h2 className="title">Gabriel Menezes</h2>
           <span className="description">Desenvolvedor & Designer</span>
           <Logo className="logo"></Logo>
-          <div className="shadow"></div>
+          {/* <div className="shadow"></div> */}
         </div>
+        <div className="icons">
+          <a
+            href="https://www.linkedin.com/in/gabrielmnzs/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="ri-linkedin-box-fill"></i>
+          </a>
+          <a
+            href="https://github.com/gabrielmnzs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="ri-github-fill"></i>
+          </a>
+          <a
+            href="https://www.instagram.com/gm.codes/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i class="ri-instagram-line"></i>
+          </a>
+        </div>
+        <footer>Em breve</footer>
       </section>
     </>
   );
